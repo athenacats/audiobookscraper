9475082abacc96c9ec1e0fd69333f77bbf2a3998
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-03 15:01:55 +0300

    Set up server.ts file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import axios from "axios";
-import cheerio from "cheerio";
+import cheerio, { load } from "cheerio";
 import cors from "cors";
 
 const app = express();
@@ -9,3 +9,27 @@ app.use(
     credentials: true,
   })
 );
+
+app.get("/scrape", async (req, res) => {
+  const url = "http://theaudiobookbay.cc/";
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+
+    const $ = load(html);
+    let audiobooks: { title: string }[] = [];
+    $(".product-list").each((index, element) => {
+      const title = $(element).text().trim();
+      audiobooks.push({ title });
+    });
+    res.json(audiobooks);
+  } catch (error) {
+    console.log("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
